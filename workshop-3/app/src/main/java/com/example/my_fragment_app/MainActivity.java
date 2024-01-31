58ebf052215bdb9e9123d99b0da9a1fac7476e54
ThomasAvans <tv.intanker@student.avans.nl>
2024-01-31 16:04:57 +0100

    Small changes

--- workshop-3/app/src/main/java/com/example/my_fragment_app/MainActivity.java
+++ workshop-3/app/src/main/java/com/example/my_fragment_app/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onItemSelect(OverviewFragment.OverviewActivity overview_activity) {
+
+    }
+
 }
